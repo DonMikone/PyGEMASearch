8dc7f98b5e4c2025b658a36a2100633693c3907d
Mike Kremer <mikelsoft@gmail.com>
2025-03-01 17:30:34 +0100

    Increased patch number.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PyGEMASearch",
-    version="0.1.3",
+    version="0.1.4",
     author="Mike Kremer",
     author_email="mikelsoft@gmail.com",
     description="A Python package to search for songs in the GEMA database.",
