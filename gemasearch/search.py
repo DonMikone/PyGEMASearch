eb044eec473f1000053470c4d602ac1f3da38906
Mike Kremer <mikelsoft@gmail.com>
2025-03-01 15:41:49 +0100

    Added first version of working code. Very simple wrapper for song search.

---
+++ gemasearch/search.py
@@ -0,0 +1,80 @@
+import requests
+
+
+class GemaMusicSearch:
+    def __init__(self):
+        self.session = None
+        self.api_url = "https://www.gema.de/portal/api/public/cloud/v1/main/repertoiresuche/suche"
+        self.init_url = "https://www.gema.de/portal/config/frontend"
+
+    def _initialize_session(self):
+
+        if self.session is None:
+            self.session = requests.Session()
+            # Load initial page to get session data
+
+            cookie_headers = {
+                "Host": "www.gema.de",
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:136.0) Gecko/20100101 Firefox/136.0",
+                "Accept": "application/json, text/plain, */*",
+                "Accept-Language": "de,en-US;q=0.7,en;q=0.3",
+                "Accept-Encoding": "gzip, deflate, br, zstd",
+                "DNT": "1",
+                "Sec-GPC": "1",
+                "Connection": "keep-alive",
+                "Referer": "https://www.gema.de/portal/app/repertoiresuche/werksuche",
+                "Sec-Fetch-Dest": "empty",
+                "Sec-Fetch-Mode": "cors",
+                "Sec-Fetch-Site": "same-origin",
+                "TE": "trailers"
+            }
+
+            r = self.session.get(self.init_url, headers=cookie_headers)
+            if r.status_code == 200:
+                api_key = r.json().get('apiKey', None)
+
+                self.session.headers.update({
+                    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:136.0) Gecko/20100101 Firefox/136.0",
+                    "Accept": "application/json, text/plain, */*",
+                    "Accept-Language": "de,en-US;q=0.7,en;q=0.3",
+                    "Content-Language": "de",
+                    "API_KEY": api_key,
+                    "Active-Role": "OPAL_NUTZER",
+                    "Content-Type": "application/json",
+                    "Origin": "https://www.gema.de",
+                    "Referer": "https://www.gema.de/portal/app/repertoiresuche/werksuche",
+                    "Connection": "keep-alive"
+                })
+                return True
+
+            else:
+                print('Could not fetch api key. Aborting!')
+                return False
+
+        # Session is initialized and active
+        return True
+
+    def search(self, title: str, page: int = 0, page_size: int = 50):
+        if not self._initialize_session():
+            print('No active session!')
+            return None
+
+        payload = {
+            "queryCriteria": [{
+                "field": "WERK_TITEL",
+                "matchOperator": "FUZZY",
+                "value": title
+            }],
+            "filters": {},
+            "pagination": {
+                "page": page,
+                "pageSize": page_size
+            }
+        }
+        try:
+            response = self.session.post(self.api_url, json=payload)
+            response.raise_for_status()
+            return response.json()
+        except requests.RequestException as e:
+            print(f"Error fetching data: {e}")
+            return None
