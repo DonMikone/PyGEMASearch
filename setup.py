83f7fb0944ee1e95c9b9e13987ea4e7df7c33112
Mike Kremer <mikelsoft@gmail.com>
2025-05-02 14:53:41 +0200

    Increased version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PyGEMASearch",
-    version="0.1.4",
+    version="0.1.5",
     author="Mike Kremer",
     author_email="mikelsoft@gmail.com",
     description="A Python package to search for songs in the GEMA database.",
